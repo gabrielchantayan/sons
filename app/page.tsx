803da68a31d85f60702b667d134b42e59b9f66bd
Gabe Chantayan <garnet638@gmail.com>
2024-12-17 00:13:56 -0500

    initial commit; enough progress so i can go to work happy in the morning

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,177 @@
-import Image from "next/image";
+'use client';
+import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider";
+import { useState } from "react";
+
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+const [playing, set_playing] = useState<Record<string, boolean>>({});
+
+const [volume, set_volume] = useState<Record<string, number>>({});
+
+const [meander, set_meander] = useState<Record<string, boolean>>({});
+
+  const toggle_audio = (audio_id: string) => {
+    // Play the sound here
+    const audio = document.getElementById(audio_id) as HTMLAudioElement;
+
+    if (audio.paused) {
+      set_playing({ ...playing, [audio_id]: true })
+      audio.play();
+    } else {
+      set_playing({ ...playing, [audio_id]: false })
+      audio.pause();
+    }
+
+  }
+
+
+  const set_item_volume = (audio_id: string, new_volume: number) => {
+
+    set_volume({ ...volume, [audio_id]: new_volume })
+
+    const audio = document.getElementById(audio_id) as HTMLAudioElement;
+    audio.volume = new_volume;
+  }
+
+
+  const meander_volume = (audio_id: string) => {
+
+    if (meander[audio_id]) {
+      set_meander({ ...meander, [audio_id]: false })
+    }
+
+    else {
+		set_meander({ ...meander, [audio_id]: true });
+    // If an item is meandering, the volume will over time change by +/- 0.4 every 10 seconds
+    let current_volume = volume[audio_id];
+    const interval_id = setInterval(() => {
+      const new_volume = current_volume + (Math.random() - 0.5) * 0.008;
+      current_volume = new_volume;
+      const audio = document.getElementById(audio_id) as HTMLAudioElement;
+      audio.volume = current_volume;
+    }, 1000 / 60); // 60 times per second
+    return () => clearInterval(interval_id);
+	}
+  }
+
+//   const SoundItem = ({ audio_name, audio_id, audio_url } : { audio_name: string, audio_id: string, audio_url: string }) => {
+
+// return(<div className='mt-8 flex gap-4'>
+// 	<p>{audio_name}</p>
+
+// 	<p>{volume[audio_id]}</p>
+
+// 	<Button onClick={() => toggle_audio(audio_id)}>{playing[audio_id] ? 'Stop' : 'Play'}</Button>
+// 	<audio id={audio_id} src={`/${audio_url}.mp3`} loop></audio>
+// 	<div className='w-96'>
+// 		{' '}
+// 		<Slider
+// 			defaultValue={[1]}
+// 			onValueChange={(v) => {
+// 				set_item_volume(audio_id, v[0]);
+// 			}}
+// 			step={0.01}
+// 			min={0}
+// 			max={1}
+// 		/>
+// 	</div>
+
+// 	<Button onClick={() => meander_volume(audio_id)}>{meander[audio_id] ? 'Stop' : 'Meander'}</Button>
+// </div>)}
+
+
+  return (
+		<div className='grid grid-rows-[20px_1fr_20px]  min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+			<main className='flex flex-col '>
+				<h1 className='text-4xl font-bold'>Sons</h1>
+				<p className='mt-4 text-2xl'>A music app for your ears</p>
+
+				<div className='mt-8 flex gap-4'>
+					<p>Subway</p>
+
+					<p>{volume['subway']}</p>
+
+					<Button onClick={() => toggle_audio('subway')}>{playing['subway'] ? 'Stop' : 'Play'}</Button>
+					<audio id='subway' src='/subway_1.mp3' loop></audio>
+					<div className='w-96'>
+						{' '}
+						<Slider
+							defaultValue={[1]}
+							onValueChange={(v) => {
+								set_item_volume('subway', v[0]);
+							}}
+							step={0.01}
+							min={0}
+							max={1}
+						/>
+					</div>
+
+					<Button onClick={() => meander_volume('subway')}>{meander['subway'] ? 'Stop' : 'Meander'}</Button>
+				</div>
+
+				<div className='mt-8 flex gap-4'>
+					<p>Rain</p>
+
+					<Button onClick={() => toggle_audio('rain')}>{playing['rain'] ? 'Stop' : 'Play'}</Button>
+					<audio id='rain' src='/rain_1.mp3' loop></audio>
+					<div className='w-96'>
+						{' '}
+						<Slider
+							defaultValue={[1]}
+							onValueChange={(v) => {
+								set_item_volume('rain', v[0]);
+							}}
+							step={0.01}
+							min={0}
+							max={1}
+						/>
+					</div>
+				</div>
+
+				<div className='mt-8 flex gap-4'>
+					<p>Thunder</p>
+
+					<Button onClick={() => toggle_audio('thunder')}>{playing['thunder'] ? 'Stop' : 'Play'}</Button>
+					<audio id='thunder' src='/thunder_1.mp3' loop></audio>
+					<div className='w-96'>
+						{' '}
+						<Slider
+							defaultValue={[1]}
+							onValueChange={(v) => {
+								set_item_volume('thunder', v[0]);
+							}}
+							step={0.01}
+							min={0}
+							max={1}
+						/>
+					</div>
+				</div>
+
+				<div className='mt-8 flex gap-4'>
+					<p>Seoul Cafe</p>
+
+					<Button onClick={() => toggle_audio('seoul_cafe')}>{playing['seoul_cafe'] ? 'Stop' : 'Play'}</Button>
+					<audio id='seoul_cafe' src='/seoul_cafe.mp3' loop></audio>
+					<div className='w-96'>
+						{' '}
+						<Slider
+							defaultValue={[1]}
+							onValueChange={(v) => {
+								set_item_volume('seoul_cafe', v[0]);
+							}}
+							step={0.01}
+							min={0}
+							max={1}
+						/>
+					</div>
+				</div>
+
+				{/* <SoundItem audio_name="Seoul Cafe" audio_id="seoul_cafe" audio_url="seoul_cafe" /> */}
+			</main>
+		</div>
   );
 }
